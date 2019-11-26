eaae1fcdb0b6c9dd33cf21fdc0336a6ac50637a4
Carlo Gilmar <carlogilmar12@gmail.com>
2019-11-25 22:51:39 -0600

    Adding guardian deps

--- mix.exs
+++ mix.exs
@@ -43,7 +43,10 @@ defmodule Star.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.3.1"}
+      {:phoenix_live_view, "~> 0.3.1"},
+      {:comeonin, "~> 5.1"},
+      {:argon2_elixir, "~> 2.0"},
+      {:guardian, "~> 1.2"}
     ]
   end
 
