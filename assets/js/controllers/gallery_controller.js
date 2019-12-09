580794e159cd20c219955f9e72e2f60db28d1b71
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-09 15:32:14 -0600

    getting gallery in controller through socket

--- assets/js/controllers/gallery_controller.js
+++ assets/js/controllers/gallery_controller.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import socket from "./../socket"
 
 export const app = new Vue({
   el:"#app",
@@ -6,6 +7,16 @@ export const app = new Vue({
   },
   created: function() {
     console.log("Vue App here!");
+    let gallery = document.getElementById("gallery").value
+    this.channel = socket.channel("gallery:join", {gallery: gallery});
+    this.channel.join()
+      .receive("ok", resp => {
+        console.log("Joined successfully");
+        console.log(resp);
+      })
+      .receive("error", resp => {
+        console.log("Unable to join", resp);
+      });
   },
   methods: {
   }
--- lib/star_web/channels/gallery_socket.ex
+++ lib/star_web/channels/gallery_socket.ex
@@ -1,8 +1,39 @@
 defmodule StarWeb.GalleryChannel do
   use Phoenix.Channel
+  alias Star.GalleryOperator
 
-  def join("gallery:join", _msg, socket) do
-    {:ok, %{}, socket}
+  def join("gallery:join", %{"gallery" => gallery_id}, socket) do
+    {:ok, get_gallery(gallery_id), socket}
+  end
+
+  defp get_gallery(gallery_id) do
+    gallery_id
+    |> String.to_integer()
+    |> GalleryOperator.get_by_id()
+    |> send_by_socket()
+  end
+
+  def send_by_socket(gallery) do
+    gallery_for_send = %{
+      "id" => gallery.id,
+      "cover" => gallery.cover,
+      "eng_desc" => gallery.eng_desc,
+      "esp_desc" => gallery.esp_desc,
+      "status" => gallery.status,
+      "title" => gallery.title
+    }
+
+    images =
+      for image <- gallery.image do
+        %{
+          "id" => image.id,
+          "url" => image.url,
+          "eng_desc" => image.eng_desc,
+          "esp_desc" => image.esp_desc
+        }
+      end
+
+    %{gallery: gallery_for_send, images: images}
   end
 
 end
