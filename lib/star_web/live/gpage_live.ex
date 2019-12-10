fece5b29ec18e0d93efff89fe472a5309ed8658b
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-09 22:27:48 -0600

    Adding function for delete gallery

--- lib/star_web/live/gpage_live.ex
+++ lib/star_web/live/gpage_live.ex
@@ -18,8 +18,15 @@ defmodule StarWeb.GpageLive do
   end
 
   def handle_event("new", _value, socket) do
-    {:ok, gallery} = GalleryOperator.create("Gallery", "Empty", "", "")
+    {:ok, gallery} = GalleryOperator.create("Gallery", "https://res.cloudinary.com/carlogilmar/image/upload/v1575604448/apprentices_journey/wip_dsixew.png", "", "")
     {:noreply, live_redirect(socket, to: "/admin/gallery/#{gallery.id}")}
+  end
+
+  def handle_event("delete", %{"gallery" => gallery}, socket) do
+    gallery = String.to_integer(gallery)
+    _ = GalleryOperator.delete(gallery)
+    socket = update(socket)
+    {:noreply, socket}
   end
 
   def handle_event("active", %{"gallery" => gallery}, socket) do
