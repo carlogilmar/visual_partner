87c8e5053791423fa398d78af588fc9d7da5869c
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-09 23:09:45 -0600

    Show every gallery in public

--- assets/js/controllers/images_controller.js
+++ assets/js/controllers/images_controller.js
@@ -1,13 +1,28 @@
 import Vue from 'vue'
 import VueCarousel from 'vue-carousel';
+import socket from "./../socket"
 Vue.use(VueCarousel);
 
 export const app = new Vue({
   el:"#app",
   data: {
+    images: [],
+    gallery: {}
   },
   created: function() {
     console.log("Vue App here!");
+    let gallery = document.getElementById("gallery").value
+    this.channel = socket.channel("gallery:join", {gallery: gallery});
+    this.channel.join()
+      .receive("ok", resp => {
+        console.log("Joined successfully");
+        console.log(resp);
+        this.images = resp.images;
+        this.gallery = resp.gallery;
+      })
+      .receive("error", resp => {
+        console.log("Unable to join", resp);
+      });
   },
   methods: {
   }
