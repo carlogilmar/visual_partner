bb31b9461adc7d6ef2cc5dfa291ae39bb9213ed0
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-10 19:59:43 -0600

    Setup update main view

--- assets/js/controllers/notes_controller.js
+++ assets/js/controllers/notes_controller.js
@@ -4,6 +4,8 @@ import socket from "./../socket"
 export const app = new Vue({
   el:"#app",
   data: {
+    note_selected: null,
+    notes: []
   },
   created: function() {
     console.log("Vue App here!");
@@ -12,11 +14,24 @@ export const app = new Vue({
       .receive("ok", resp => {
         console.log("Joined successfully");
         console.log(resp);
+        this.notes = resp.notes;
       })
       .receive("error", resp => {
         console.log("Unable to join", resp);
       });
   },
   methods: {
+    show_note:function(note_id){
+      console.log("Mostrando nota");
+      this.channel.push("notes:show", {note: note_id})
+        .receive('ok', (res) => {
+          console.log("DONE");
+          console.log(res);
+          this.note_selected = res.note;
+        })
+        .receive("error", resp => {
+          console.log("ERROR");
+        });
+    }
   }
 });
--- lib/star_web/channels/notes_socket.ex
+++ lib/star_web/channels/notes_socket.ex
@@ -1,9 +1,30 @@
 defmodule StarWeb.NotesChannel do
   use Phoenix.Channel
   alias Star.AnalyticsUtil
+  alias Star.NoteOperator
 
   def join("notes:join", _msg, socket) do
-    {:ok, %{}, socket}
+    {:ok, %{notes: get_notes()}, socket}
+  end
+
+  def handle_in(
+    "notes:show",
+    %{"note" => note},
+    socket
+  ) do
+		note = NoteOperator.get_by_id(note)
+		note = %{id: note.id, title: note.title, body: note.body}
+    {:reply, {:ok, %{note: note}}, socket}
+  end
+
+  defp get_notes do
+    notes = NoteOperator.get_all()
+    for note <- notes do
+      %{
+        "id" => note.id,
+        "title" => note.title
+      }
+    end
   end
 
 end
