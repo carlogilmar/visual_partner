3c2aff49f8aa1c4d14cfaaa6f28907c30e968b7a
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-11 19:14:29 -0600

    Building the view for create and get the comments in view

--- assets/js/controllers/note_controller.js
+++ assets/js/controllers/note_controller.js
@@ -2,22 +2,39 @@ import Vue from 'vue'
 import socket from "./../socket"
 
 export const app = new Vue({
-	el:"#app",
-	data: {
-    note: {}
-	},
-	created: function() {
-		console.log("Vue App here!");
-		let note = document.getElementById("note").value
-		this.channel = socket.channel("note:join", {note: note});
-		this.channel.join()
-			.receive("ok", resp => {
-				console.log("Joined successfully");
-				console.log(resp);
-				this.note = resp.note;
-			})
-			.receive("error", resp => {
-				console.log("Unable to join", resp);
-			});
-	}
+  el:"#app",
+  data: {
+    note: {},
+    username: "",
+    comment: "",
+    comments: []
+  },
+  created: function() {
+    console.log("Vue App here!");
+    let note = document.getElementById("note").value
+    this.channel = socket.channel("note:join", {note: note});
+    this.channel.join()
+      .receive("ok", resp => {
+        console.log("Joined successfully");
+        console.log(resp);
+        this.note = resp.note;
+        this.comments = resp.comments;
+      })
+      .receive("error", resp => {
+        console.log("Unable to join", resp);
+      });
+  },
+  methods:{
+    create_comment:function(){
+      console.log("Que onda!");
+      let id = document.getElementById("note").value
+      this.channel.push("note:new_comment", {id: id, username: this.username, comment: this.comment})
+        .receive('ok', (res) => {
+          this.comments = res.comments;
+        })
+        .receive("error", resp => {
+          console.log("ERROR");
+        });
+    }
+  }
 });
--- lib/star_web/channels/note_socket.ex
+++ lib/star_web/channels/note_socket.ex
@@ -6,6 +6,15 @@ defmodule StarWeb.NoteChannel do
     {:ok, %{note: get_note(id)}, socket}
   end
 
+  def handle_in(
+    "note:new_comment",
+    %{"id" => id, "username" => username, "comment" => comment},
+    socket
+  ) do
+    IO.puts "\n Que onda ======> \n"
+    {:reply, {:ok, %{note: get_note(id)}}, socket}
+  end
+
   def get_note(id) do
     id = String.to_integer(id)
     note = NoteOperator.get_by_id(id)
