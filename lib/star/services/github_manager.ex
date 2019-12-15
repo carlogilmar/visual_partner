517e4931b0aa91d3a0de4332736c532240434de6
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-14 22:52:14 -0600

    Implementing http poison for get github response

---
+++ lib/star/services/github_manager.ex
@@ -0,0 +1,32 @@
+defmodule Star.GitHubManager do
+  @github_api Application.get_env(:star, StarWeb.Endpoint)[:github_api_key]
+  @repo_url "https://api.github.com/repos/carlogilmar/star/commits"
+
+  def get_commits() do
+    headers = [
+      {:"Authorization", @github_api},
+      {:"Content-Type", "application/vnd.github.mercy-preview+json"}]
+    {:ok, response} = HTTPoison.get(@repo_url, headers)
+    decode_response.(response.body)
+  end
+
+  defp decode_response do
+    fn response ->
+      {:ok, body} = Poison.decode(response)
+      parse_commit_response(body)
+    end
+  end
+
+  defp parse_commit_response(response) do
+    for commit <- response do
+      %{
+        desc: commit["commit"]["message"],
+        date: commit["commit"]["author"]["date"],
+        url: commit["html_url"],
+        author: commit["author"]["login"],
+        author_profile: commit["author"]["url"]
+      }
+    end
+  end
+end
+
---
+++ lib/star/services/request_manager.ex
@@ -0,0 +1,23 @@
+defmodule Star.RequestManager do
+
+  def post(url, params, content_type) do
+    case HTTPoison.post(url, Poison.encode!(params), content_type) do
+      {:ok, response} ->
+        case response.status_code do
+          200 -> decode_response.(response.body)
+          status when status in 400..499 -> :client_error
+          status when status in 500..599 -> :server_error
+        end
+
+      _error ->
+        :unknown_error
+    end
+  end
+
+  defp decode_response do
+    fn response ->
+      {:ok, body} = Poison.decode(response)
+      body
+    end
+  end
+end
