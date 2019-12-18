ce4e4e26b6b9b11bdf10b1c7b7966ffb5c0da4f6
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-17 23:09:37 -0600

    Getting user in live view

--- lib/star_web/live/register_live.ex
+++ lib/star_web/live/register_live.ex
@@ -2,6 +2,7 @@ defmodule StarWeb.RegisterLive do
 
   use Phoenix.LiveView
   alias StarWeb.RegisterView
+  alias Star.UserOperator
 
   def render(assigns) do
     RegisterView.render("index.html", assigns)
@@ -11,7 +12,9 @@ defmodule StarWeb.RegisterLive do
     {:ok, socket}
   end
 
-  def handle_params(%{"id" => user_id}, _url, socket) do
+  def handle_params(%{"id" => user_uuid}, _url, socket) do
+    user = UserOperator.get_by_identifier(user_uuid)
+    socket = socket |> assign(:user, user)
     {:noreply, socket}
   end
 
