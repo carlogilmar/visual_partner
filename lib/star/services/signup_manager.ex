b53b050e97a6efc954dde1a8b38e64b23ef74671
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-17 22:41:46 -0600

    Adding signup manager for manager when create an user

---
+++ lib/star/services/signup_manager.ex
@@ -0,0 +1,36 @@
+defmodule Star.SignupManager do
+	alias  Star.EmailerSenderOperator
+  alias Star.UserOperator
+  @user_role "USER"
+
+  def create_user(params) do
+    params
+    |> find_as_suscriptor()
+    |> create_or_update_to_user()
+    |> send_email_for_active()
+  end
+
+  def find_as_suscriptor(params) do
+    user = UserOperator.get_by_email(params["email"])
+    case user do
+      nil -> {params, :to_create}
+      _user -> {user, :to_update}
+    end
+  end
+
+  def create_or_update_to_user({params, :to_create}) do
+    {:ok, user} =
+      UserOperator.create_user(params["email"], "", "", @user_role)
+    user
+  end
+
+  def create_or_update_to_user({user, :to_update}) do
+    {:ok, user} = UserOperator.update(user.id, %{role: "USER"})
+    user
+  end
+
+	def send_email_for_active(user) do
+		_ = EmailerSenderOperator.send_signup_email(user.email)
+	end
+
+end
