e6ff508a329b57601458fe1b22af4b8c6822115b
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-23 16:53:47 -0600

    updating new url in prod

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :star, StarWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [host: "starcg.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  base_url: "https://starcg.herokuapp.com/"
+  base_url: "https://apprenticesjourney.herokuapp.com/"
 
 # Do not print debug messages in production
 config :logger, level: :info
