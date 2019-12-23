353b6b93fbedf18203a2d6c59c2856658e16ec25
Carlo Gilmar <carlogilmar12@gmail.com>
2019-12-23 16:41:18 -0600

    Removing innecesary carousel

--- assets/js/controllers/apprentice_controller.js
+++ assets/js/controllers/apprentice_controller.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import socket from "./../socket"
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'slick-carousel/slick/slick-theme.css'
-import 'slick-carousel/slick/slick-theme.css'
-Vue.use(VueSlickCarousel)
-
 
 export const app = new Vue({
   el:"#app",
@@ -12,13 +7,5 @@ export const app = new Vue({
   },
   created: function() {
     console.log("Vue App here!");
-    //this.channel = socket.channel("apprenti:join", {});
-    //this.channel.join()
-    //  .receive("ok", resp => {
-    //    console.log("Joined successfully, getting timezone data!");
-    //  })
-    //  .receive("error", resp => {
-    //    console.log("Unable to join", resp);
-    //  });
   }
 });
