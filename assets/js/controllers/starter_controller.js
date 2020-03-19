edb3bed38df69b2fbf3a08537ccb49c2cdad6985
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-19 02:23:46 -0600

    modify landing page

--- assets/js/controllers/starter_controller.js
+++ assets/js/controllers/starter_controller.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import socket from "./../socket"
+import Carousel3d from 'vue-carousel-3d';
+import VueStar from 'vue-star'
+Vue.use(Carousel3d);
+Vue.component('VueStar', VueStar)
 
 export const app = new Vue({
   el:"#app",
