7e342e29b2209808189f28b725a7a859434ce350
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-22 14:05:04 -0600

    updating artwork album and gallery

--- lib/star_web/live/gallery_live.ex
+++ lib/star_web/live/gallery_live.ex
@@ -12,14 +12,30 @@ defmodule StarWeb.GalleryLive do
   end
 
   def handle_params(%{"type" => type}, _url, socket) do
+    artwork_type = get_artwork_type(type)
     type = String.upcase(type)
     galleries = GalleryOperator.find_by_status(true, type)
-    socket = socket |> assign(:galleries, galleries)
+    socket = socket |> assign(:galleries, galleries) |> assign(:type, artwork_type)
 
     {:noreply, socket}
   end
 
   def handle_event("redirect_url", %{"uri_val" => uri_val}, socket) do
     {:noreply, live_redirect(socket, to: uri_val)}
+  end
+
+  defp get_artwork_type(type) do
+    types = %{
+      "partnership" => "Visual Partnership",
+      "funny" => "Visual Stuff",
+      "community" => "Communtiy",
+      "event" => "Eventos",
+      "meetings" => "Meetings",
+      "resources" => "Learning"
+    }
+    case types[type] do
+      nil -> "@visual_partner"
+      artwork -> artwork
+    end
   end
 end
