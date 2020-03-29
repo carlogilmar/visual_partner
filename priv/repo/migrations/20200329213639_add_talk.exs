43641e49d4162ae79242c713ec028e320e1b290f
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-29 15:39:38 -0600

    Adding migration

---
+++ priv/repo/migrations/20200329213639_add_talk.exs
@@ -0,0 +1,14 @@
+defmodule Star.Repo.Migrations.AddTalk do
+  use Ecto.Migration
+
+  def change do
+    create table(:talks, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :title, :string
+      add :url, :string
+      add :cover, :string
+      add :clicks, :integer
+      add :draft, :boolean
+    end
+  end
+end
