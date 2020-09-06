27b658a1dc8b11fee0b4bf7a76e6355b7edda162
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-06 02:53:04 -0500

    Setup calendar vue component

--- assets/js/controllers/tasks_controller.js
+++ assets/js/controllers/tasks_controller.js
@@ -1,21 +1,41 @@
 import Vue from 'vue'
 import socket from "./../socket"
+import VCalendar from 'v-calendar';
+Vue.use(VCalendar)
 
 export const app = new Vue({
-  el:"#app",
-  data: {
-  },
-  created: function() {
-    console.log("Vue App here! Tasks Controller");
-    this.channel = socket.channel("tasks:join", {});
-    this.channel.join()
-      .receive("ok", resp => {
-        console.log("Joined successfully");
-        console.log(resp);
-        this.notes = resp.notes;
-      })
-      .receive("error", resp => {
-        console.log("Unable to join", resp);
-      });
-  }
+	el:"#app",
+	data: {
+		attributes: [],
+	},
+	created: function() {
+		console.log("Vue App here! Tasks Controller");
+		this.channel = socket.channel("tasks:join", {});
+		this.channel.join()
+			.receive("ok", resp => {
+				console.log("Joined successfully");
+				console.log(resp);
+				this.tasks = resp.tasks;
+				this.generate_calendar();
+			})
+			.receive("error", resp => {
+				console.log("Unable to join", resp);
+			});
+	},
+	methods:{
+		generate_calendar: function(){
+			let dates = [];
+			for(let index=0; index < (this.tasks.length); index++){
+				let date = this.tasks[index];
+				let date_for_show = {
+					highlight: {backgroundColor: '#000000'},
+					contentStyle: {color: '#00000'},
+					popover: { label: date.title},
+					dates: [new Date(date.year, (date.month-1), date.day)],
+				}
+				dates.push(date_for_show)
+			}
+			this.attributes = dates;
+		},
+	}
 });
--- lib/star_web/channels/tasks_socket.ex
+++ lib/star_web/channels/tasks_socket.ex
@@ -1,7 +1,28 @@
 defmodule StarWeb.TasksChannel do
-  use Phoenix.Channel
+	use Phoenix.Channel
+	alias Star.TaskOperator
 
-  def join("tasks:join", _msg, socket) do
-    {:ok, %{}, socket}
-  end
+	def join("tasks:join", _msg, socket) do
+		tasks = get_tasks()
+		{:ok, %{tasks: tasks}, socket}
+	end
+
+	def get_tasks do
+		tasks = TaskOperator.get_monthly_tasks()
+		parse_tasks(tasks)
+	end
+
+	def parse_tasks(tasks) do
+		for task <- tasks do
+			%{
+				id: task.id,
+				title: task.title,
+				status: task.status,
+				description: task.description,
+				year: task.inserted_at.year,
+				month: task.inserted_at.month,
+				day: task.inserted_at.day
+			}
+		end
+	end
 end
