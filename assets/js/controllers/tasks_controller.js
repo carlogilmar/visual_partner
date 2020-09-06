afae70cec494fdfe0e57bb7793cb6f0d95f986e9
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-06 02:23:06 -0500

    Setup channel join connection

--- assets/js/controllers/tasks_controller.js
+++ assets/js/controllers/tasks_controller.js
@@ -7,5 +7,15 @@ export const app = new Vue({
   },
   created: function() {
     console.log("Vue App here! Tasks Controller");
+    this.channel = socket.channel("tasks:join", {});
+    this.channel.join()
+      .receive("ok", resp => {
+        console.log("Joined successfully");
+        console.log(resp);
+        this.notes = resp.notes;
+      })
+      .receive("error", resp => {
+        console.log("Unable to join", resp);
+      });
   }
 });
--- lib/star_web/channels/tasks_socket.ex
+++ lib/star_web/channels/tasks_socket.ex
@@ -1,7 +1,7 @@
 defmodule StarWeb.TasksChannel do
   use Phoenix.Channel
 
-  def join("roster:join", _msg, socket) do
+  def join("tasks:join", _msg, socket) do
     {:ok, %{}, socket}
   end
 end
