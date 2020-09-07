dce1d8bf2d9f1a9d06b6a9839dd73cd2002e2c66
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-06 21:39:01 -0500

    Adding charts examples in main view!

--- assets/js/controllers/tasks_controller.js
+++ assets/js/controllers/tasks_controller.js
@@ -2,20 +2,57 @@ import Vue from 'vue';
 import socket from "./../socket";
 import VCalendar from 'v-calendar';
 import draggable from 'vuedraggable';
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
 Vue.use(VCalendar)
+Vue.component('apexchart', VueApexCharts)
 
 export const app = new Vue({
 	el:"#app",
-  components: {
-    draggable,
-  },
+	components: {
+		draggable,
+	},
 	data: {
 		attributes: [],
-    todo: [],
-    doing: [],
-    done: [],
-    task: { title: "", description: "...", date: new Date()},
-		element: {}
+		todo: [],
+		doing: [],
+		done: [],
+		task: { title: "", description: "...", date: new Date()},
+		element: {},
+		// Barra circular
+		chartOptions: {
+			chart: {
+				height: 150,
+				type: 'radialBar',
+			},
+			plotOptions: {
+				radialBar: {
+					hollow: {
+						size: '40%', // Ancho de circulo
+					}
+				},
+			},
+			labels: ['Done Tasks'],
+		},
+		series: [35],
+		chartOptions2: {
+			chart: {
+				type: 'pie',
+			},
+			labels: ['To Do', 'Doing', 'Done'],
+			responsive: [{
+				breakpoint: 480,
+				options: {
+					chart: {
+						width: 200
+					},
+					legend: {
+						position: 'bottom'
+					}
+				}
+			}]
+		},
+		series2: [5, 3, 8],
 	},
 	created: function() {
 		console.log("Vue App here! Tasks Controller");
@@ -23,12 +60,12 @@ export const app = new Vue({
 		this.channel.join()
 			.receive("ok", resp => {
 				console.log("Joined successfully");
-        console.log(resp);
+				console.log(resp);
 				this.tasks = resp.tasks;
 				this.generate_calendar();
-        this.todo = resp.todo;
-        this.doing = resp.doing;
-        this.done = resp.done;
+				this.todo = resp.todo;
+				this.doing = resp.doing;
+				this.done = resp.done;
 			})
 			.receive("error", resp => {
 				console.log("Unable to join", resp);
@@ -49,96 +86,96 @@ export const app = new Vue({
 			}
 			this.attributes = dates;
 		},
-    todo_log: function(evt) {
-      if(evt.added){
-        this.move_task(evt.added.element.id, "TO DO")
-      }
-    },
-    doing_log: function(evt) {
-      if(evt.added){
-        this.move_task(evt.added.element.id, "DOING")
-      }
-    },
-    done_log: function(evt) {
-      if(evt.added){
-        this.move_task(evt.added.element.id, "DONE")
-      }
-    },
-    move_task: function(id, value){
-      this.channel.push("tasks:move_task", {id: id, value: value})
-        .receive('ok', (resp) => {
-          this.todo = resp.todo;
-          this.doing = resp.doing;
-          this.done = resp.done;
-        })
-        .receive("error", resp => {
-          console.log("ERROR");
-        });
-    },
-    unpin: function(id){
-      console.log("unpinn");
-      this.set_pinned(id, false);
-    },
-    pin: function(id){
-      console.log("pinn");
-      this.set_pinned(id, true);
-    },
-    set_pinned: function(id, value){
-      this.channel.push("tasks:pinned", {id: id, value: value})
-        .receive('ok', (resp) => {
-          console.log("pinned");
-          this.todo = resp.todo;
-          this.doing = resp.doing;
-          this.done = resp.done;
-        })
-        .receive("error", resp => {
-          console.log("ERROR");
-        });
-    },
+		todo_log: function(evt) {
+			if(evt.added){
+				this.move_task(evt.added.element.id, "TO DO")
+			}
+		},
+		doing_log: function(evt) {
+			if(evt.added){
+				this.move_task(evt.added.element.id, "DOING")
+			}
+		},
+		done_log: function(evt) {
+			if(evt.added){
+				this.move_task(evt.added.element.id, "DONE")
+			}
+		},
+		move_task: function(id, value){
+			this.channel.push("tasks:move_task", {id: id, value: value})
+				.receive('ok', (resp) => {
+					this.todo = resp.todo;
+					this.doing = resp.doing;
+					this.done = resp.done;
+				})
+				.receive("error", resp => {
+					console.log("ERROR");
+				});
+		},
+		unpin: function(id){
+			console.log("unpinn");
+			this.set_pinned(id, false);
+		},
+		pin: function(id){
+			console.log("pinn");
+			this.set_pinned(id, true);
+		},
+		set_pinned: function(id, value){
+			this.channel.push("tasks:pinned", {id: id, value: value})
+				.receive('ok', (resp) => {
+					console.log("pinned");
+					this.todo = resp.todo;
+					this.doing = resp.doing;
+					this.done = resp.done;
+				})
+				.receive("error", resp => {
+					console.log("ERROR");
+				});
+		},
 		show_modal: function(element){
 			this.element = element;
 		},
 		update:function(value, id, attr){
-      this.channel.push("tasks:update", {id: id, value: value, attr: attr})
-        .receive('ok', (resp) => {
-          console.log("DONE");
-        })
-        .receive("error", resp => {
-          console.log("ERROR");
-        });
+			this.channel.push("tasks:update", {id: id, value: value, attr: attr})
+				.receive('ok', (resp) => {
+					console.log("DONE");
+				})
+				.receive("error", resp => {
+					console.log("ERROR");
+				});
 		},
-    onSubmit: function(){
-      this.channel.push("tasks:new", this.task)
-        .receive('ok', (resp) => {
-          console.log("DONE");
-          this.tasks = resp.tasks;
-          this.generate_calendar();
-          this.todo = resp.todo;
-          this.doing = resp.doing;
-          this.done = resp.done;
-          // Cleaning Object
-          this.task.title = "";
-          this.task.description = "...";
-          this.task.date = new Date();
-        })
-        .receive("error", resp => {
-          console.log("ERROR");
-        });
-    },
-    delete_task: function(task_id){
-      this.channel.push("tasks:delete", task_id)
-        .receive('ok', (resp) => {
-          console.log("DONE");
-          this.tasks = resp.tasks;
-          this.generate_calendar();
-          this.todo = resp.todo;
-          this.doing = resp.doing;
-          this.done = resp.done;
-          $('#exampleModal').modal('hide');
-        })
-        .receive("error", resp => {
-          console.log("ERROR");
-        });
-    }
+		onSubmit: function(){
+			this.channel.push("tasks:new", this.task)
+				.receive('ok', (resp) => {
+					console.log("DONE");
+					this.tasks = resp.tasks;
+					this.generate_calendar();
+					this.todo = resp.todo;
+					this.doing = resp.doing;
+					this.done = resp.done;
+					// Cleaning Object
+					this.task.title = "";
+					this.task.description = "...";
+					this.task.date = new Date();
+				})
+				.receive("error", resp => {
+					console.log("ERROR");
+				});
+		},
+		delete_task: function(task_id){
+			this.channel.push("tasks:delete", task_id)
+				.receive('ok', (resp) => {
+					console.log("DONE");
+					this.tasks = resp.tasks;
+					this.generate_calendar();
+					this.todo = resp.todo;
+					this.doing = resp.doing;
+					this.done = resp.done;
+					$('#exampleModal').modal('hide');
+				})
+				.receive("error", resp => {
+					console.log("ERROR");
+				});
+		}
 	}
 });
