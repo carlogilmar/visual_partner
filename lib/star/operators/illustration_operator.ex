10e01d5d8e5ef4624591aee355b87e35567c9339
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-07 08:21:45 -0500

    Adding illustration operator

---
+++ lib/star/operators/illustration_operator.ex
@@ -0,0 +1,36 @@
+defmodule Star.IllustrationOperator do
+  alias Star.DeliverableOperator
+  alias Star.Illustration
+  alias Star.Repo
+
+  def create(deliverable_id, url) do
+    deliverable = DeliverableOperator.get_by_id(deliverable_id)
+     %Illustration{
+       url: url,
+       deliverable: deliverable
+      }
+    |> Repo.insert()
+  end
+
+  def get_by_id(id) do
+    Repo.get(Illustration, id)
+  end
+
+  def get_all do
+    Repo.all(Illustration)
+  end
+
+  def delete(id) do
+    model = get_by_id(id)
+    Repo.delete(model)
+  end
+
+  def update(id, attrs) do
+    model = get_by_id(id)
+
+    model
+    |> Illustration.changeset(attrs)
+    |> Repo.update()
+  end
+
+end
