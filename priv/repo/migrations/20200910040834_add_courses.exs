4236a5a7a0676469d46f70b149a88e5160d1310d
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-10 00:01:09 -0500

    Adding more schemas

--- priv/repo/migrations/20200910040834_add_courses.exs
+++ priv/repo/migrations/20200910040834_add_courses.exs
@@ -18,5 +18,38 @@ defmodule Star.Repo.Migrations.AddCourses do
       add :hours, :integer
       timestamps()
     end
+
+    create table(:course_sessions, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :feedback, :string
+      add :session_date, :naive_datetime
+      add :type, :string
+      add :course_id, references(:courses, on_delete: :delete_all)
+      timestamps()
+    end
+
+    create table(:agenda_items, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :title, :string
+      add :status, :boolean
+      add :course_session_id, references(:course_sessions, on_delete: :delete_all)
+      timestamps()
+    end
+
+    create table(:promo_illustrations, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :title, :string
+      add :url, :string
+      add :course_session_id, references(:course_sessions, on_delete: :delete_all)
+      timestamps()
+    end
+
+    create table(:enrollments, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :course_session_id, references(:course_sessions, on_delete: :delete_all)
+      add :user_id, references(:users, on_delete: :delete_all)
+      add :status, :string
+      timestamps()
+    end
   end
 end
