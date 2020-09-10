a2690de0d047f125d04b43cad9afd25bd8ec0a51
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-10 00:07:57 -0500

    Adding course session operator

---
+++ lib/star/operators/course_session_operator.ex
@@ -0,0 +1,35 @@
+defmodule Star.CourseSessionOperator do
+  alias Star.CourseOperator
+  alias Star.CourseSession
+  alias Star.Repo
+
+  def create(course_id) do
+    course = CourseOperator.get_by_id(course_id)
+     %CourseSession{
+       course: course
+      }
+    |> Repo.insert()
+  end
+
+  def get_by_id(id) do
+    Repo.get(CourseSession, id)
+  end
+
+  def get_all do
+    Repo.all(CourseSession)
+  end
+
+  def delete(id) do
+    model = get_by_id(id)
+    Repo.delete(model)
+  end
+
+  def update(id, attrs) do
+    model = get_by_id(id)
+
+    model
+    |> CourseSession.changeset(attrs)
+    |> Repo.update()
+  end
+
+end
