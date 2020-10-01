75e2411d8a7476ff29d9acd0d7820467b911b0f2
Carlo Gilmar <carlogilmar12@gmail.com>
2020-10-01 01:58:15 -0500

    Implement session updating flow

--- assets/js/controllers/course_session_controller.js
+++ assets/js/controllers/course_session_controller.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import socket from "./../socket"
+import VCalendar from 'v-calendar';
+Vue.use(VCalendar)
 
 export const app = new Vue({
   el:"#app",
@@ -20,7 +22,12 @@ export const app = new Vue({
       .receive("error", resp => {
         console.log("Unable to join", resp);
       });
-  },
+	},
+	watch:{
+		'session_selected.session_date'(session_date){
+			this.update(session_date, this.session_selected.id, 'session_date' )
+		}
+	},
   methods: {
     new_session: function(){
       let course = document.getElementById("course").value;
@@ -33,9 +40,40 @@ export const app = new Vue({
           console.log("ERROR");
         });
     },
-    show_session: function(){
+    show_session: function(id){
       console.log("Nueva sesión");
       this.session_selected = 1;
+      this.channel.push("session:show", {session: id})
+        .receive('ok', (resp) => {
+          console.log("DONE");
+          console.log(resp);
+					this.parse_session(resp.session);
+        })
+        .receive("error", resp => {
+          console.log("ERROR");
+        });
     },
-  }
+		parse_session: function(session){
+			if(!session.session_date){
+				session.session_date = new Date();
+			}else{
+				session.session_date = new Date(session.session_date);
+			}
+			this.session_selected = session;
+		},
+    algo: function(){
+    console.log("me ves?");
+    console.log(this.session_selected)
+    this.update(this.session_selected.session_date, this.session_selected.id, 'session_date' )
+    },
+    update: function(value, id, attr){
+      this.channel.push("session:update", {value: value, id: id, attr: attr})
+        .receive('ok', (resp) => {
+          console.log("DONE");
+        })
+        .receive("error", resp => {
+          console.log("ERROR");
+        });
+    },
+	}
 });
--- lib/star_web/channels/session_socket.ex
+++ lib/star_web/channels/session_socket.ex
@@ -1,45 +1,75 @@
 defmodule StarWeb.SessionChannel do
-  use Phoenix.Channel
-  alias Star.CourseOperator
-  alias Star.CourseSessionOperator
+	use Phoenix.Channel
+	alias Star.CourseOperator
+	alias Star.CourseSessionOperator
 
-  def join("session:join", %{"course" => id}, socket) do
-    course_id = String.to_integer(id)
-    course = CourseOperator.get_by_id(course_id)
-    sessions = get_course_sessions(course)
-    {:ok, %{sessions: sessions, title: course.title}, socket}
-  end
+	def join("session:join", %{"course" => id}, socket) do
+		course_id = String.to_integer(id)
+		course = CourseOperator.get_by_id(course_id)
+		sessions = get_course_sessions(course)
+		{:ok, %{sessions: sessions, title: course.title}, socket}
+	end
 
-  def handle_in(
-        "session:new",
-        %{"course" => course_id},
-        socket
-      ) do
-    course_id = String.to_integer(course_id)
-    CourseSessionOperator.create(course_id)
-    course = CourseOperator.get_by_id(course_id)
-    sessions = get_course_sessions(course)
-    {:reply, {:ok, %{sessions: sessions}}, socket}
-  end
+	def handle_in(
+		"session:new",
+		%{"course" => course_id},
+		socket
+	) do
+		course_id = String.to_integer(course_id)
+		CourseSessionOperator.create(course_id)
+		course = CourseOperator.get_by_id(course_id)
+		sessions = get_course_sessions(course)
+		{:reply, {:ok, %{sessions: sessions}}, socket}
+	end
 
-  def handle_in(
-        "course:update",
-        %{"attr" => attr, "id" => id, "value" => value},
-        socket
-      ) do
-    attrs = Map.new([{String.to_atom(attr), value}])
-    {:ok, _model} = CourseOperator.update(id, attrs)
-    {:reply, {:ok, %{}}, socket}
-  end
+	def handle_in(
+		"session:show",
+		%{"session" => session_id},
+		socket
+	) do
+		session =
+			session_id
+			|> CourseSessionOperator.get_by_id()
+			|> get_session()
+		{:reply, {:ok, %{session: session}}, socket}
+	end
 
-  defp get_course_sessions(course) do
-    Enum.into(course.course_session, [], fn session ->
-      %{
-        id: session.id,
-        type: session.type,
-        session_date: session.session_date
-      }
-    end)
-  end
+	def handle_in(
+		"session:update",
+		%{"attr" => attr, "id" => id, "value" => value},
+		socket
+	) do
+		attrs = Map.new([{String.to_atom(attr), value}])
+		{:ok, _model} = CourseSessionOperator.update(id, attrs)
+		{:reply, {:ok, %{}}, socket}
+	end
+
+	defp get_session(session) do
+		%{
+			id: session.id,
+			type: session.type,
+			session_date: session.session_date
+		}
+	end
+
+	defp get_course_sessions(course) do
+		Enum.into(course.course_session, [], fn session ->
+			%{
+				id: session.id,
+				type: session.type,
+				session_date: parse_date(session.session_date)
+			}
+		end)
+	end
+
+	defp parse_date(nil) do
+		"Por definir fecha"
+	end
+
+	defp parse_date(date) do
+		date
+		|> NaiveDateTime.to_date()
+		|> Date.to_string()
+	end
 
 end
