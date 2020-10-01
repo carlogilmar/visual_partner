8fe5954cbfe63418f9ec98eb42fdad6d87678819
Carlo Gilmar <carlogilmar12@gmail.com>
2020-10-01 14:33:02 -0500

    Getting Course Sessions Opened in user home

--- lib/star/operators/course_session_operator.ex
+++ lib/star/operators/course_session_operator.ex
@@ -1,4 +1,5 @@
 defmodule Star.CourseSessionOperator do
+  import Ecto.Query, only: [from: 2]
   alias Star.CourseOperator
   alias Star.CourseSession
   alias Star.Repo
@@ -18,6 +19,11 @@ defmodule Star.CourseSessionOperator do
 
   def get_all do
     Repo.all(CourseSession)
+  end
+
+  def get_all_by_status(status) do
+    query = from(n in CourseSession, where: n.status == ^status, order_by: [desc: n.inserted_at])
+    Repo.all(query) |> Repo.preload([:course])
   end
 
   def delete(id) do
--- lib/star_web/controllers/user_controller.ex
+++ lib/star_web/controllers/user_controller.ex
@@ -1,7 +1,9 @@
 defmodule StarWeb.UserController do
   use StarWeb, :controller
+  alias Star.CourseSessionOperator
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    courses = CourseSessionOperator.get_all_by_status("OPEN")
+    render(conn, "index.html", user: conn.private[:guardian_default_resource], courses: courses)
   end
 end
