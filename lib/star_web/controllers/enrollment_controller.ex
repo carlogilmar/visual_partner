2363fd34892eb03c84b4f98d888b8dabf8f2b1ac
Carlo Gilmar <carlogilmar12@gmail.com>
2020-10-02 00:05:10 -0500

    Validating enrollment in view

--- lib/star_web/controllers/enrollment_controller.ex
+++ lib/star_web/controllers/enrollment_controller.ex
@@ -1,9 +1,13 @@
 defmodule StarWeb.EnrollmentController do
   use StarWeb, :controller
   alias Star.CourseSessionOperator
+  alias Star.EnrollmentOperator
 
   def index(conn, %{"id" => course_session_id}) do
-    course_session = CourseSessionOperator.get_by_id(String.to_integer(course_session_id))
-    render(conn, "index.html", user: conn.private[:guardian_default_resource], course_session: course_session)
+    course_session_id = String.to_integer(course_session_id)
+    user = conn.private[:guardian_default_resource]
+    course_session = CourseSessionOperator.get_by_id(course_session_id)
+    enrollment = EnrollmentOperator.get_by_course_session_and_user(course_session_id, user.identifier)
+    render(conn, "index.html", user: user, course_session: course_session, enrollment: enrollment)
   end
 end
