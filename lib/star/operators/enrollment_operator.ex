567e07decea24835459a6083a4a1ca713af053d5
Carlo Gilmar <carlogilmar12@gmail.com>
2020-10-03 21:28:53 -0500

    Add function to get enrollemtns by user id and status

--- lib/star/operators/enrollment_operator.ex
+++ lib/star/operators/enrollment_operator.ex
@@ -33,10 +33,6 @@ defmodule Star.EnrollmentOperator do
     Repo.get(Enrollment, id) |> Repo.preload([:user, course_session: [:course]])
   end
 
-  def get_all do
-    Repo.all(Enrollment)
-  end
-
   def get_by_course_session_and_user(course_session_id, user_identifier) do
     user = UserOperator.get_by_identifier(user_identifier)
 
@@ -52,6 +48,15 @@ defmodule Star.EnrollmentOperator do
     query = from(e in Enrollment, where: e.user_id == ^user_id)
     enrollments = query |> Repo.all() |> Repo.preload(course_session: [:course])
     Enum.filter(enrollments, fn enrollment -> enrollment.course_session.status == status end)
+  end
+
+  def get_all do
+    Repo.all(Enrollment)
+  end
+
+  def get_all(user_id, status) do
+    query = from(e in Enrollment, where: e.user_id == ^user_id and e.status == ^status)
+    query |> Repo.all() |> Repo.preload(course_session: [:course])
   end
 
   def delete(id) do
